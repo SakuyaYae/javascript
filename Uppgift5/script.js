b6c43955aba62b9173017cba1930ee7c14e8b680
SakuyaYae <darkreimu@outlook.com>
2021-08-26 12:12:07 +0200

    altered functions and added getNr and  addFruits
    ShowFruits alter to make getNr have alterd functionality

--- Uppgift5/script.js
+++ Uppgift5/script.js
@@ -5,6 +5,7 @@ var msgElem; // ...
 var inputElem; // stores inputs in a List
 var fruitNames; // a list that stores fruitnames
 var fruitNr // stores the number of chosen fruit
+var selFruitElem
 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd.
 // Initiering av globala variabler samt koppling avfunktioner till knapparna.
@@ -15,37 +16,52 @@ function init() {
     inputElem[3] = document.getElementById("input3")
 
     msgElem = document.getElementById("message");
-
-    fruitNames = ["Ingen frukt", "Äppel", "bannan", "citron", "apelsin", "päron"];
+    selFruitElem = document.getElementById("selectedFruits")
+    fruitNames = ["Ingen frukt", "äppel", "bannan", "citron", "apelsin", "päron"];
     fruitNr = 0;
 
     document.getElementById("btn1").onclick = showFruit;
     document.getElementById("btn2").onclick = checkName;
+    document.getElementById("btn3").onclick = addFruits;
 } // End init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 
 
-function showFruit() {
-// The function checks the imput of the first text feld and alters the img on the webbpage if the input was a nummber that 
-// coresponds to a img of a fruit. if the input is a wrong number or if its NaN it tells the user
-    var nr; // takes the input from a varable an converts it int a number
-    var fruitUrl; // stores a reffrens that is used to alter the img on the webbpage
 
-    nr = inputElem[1];
-    nr = Number(inputElem[1].value)
-    fruitUrl = document.getElementById("fruitImg");
+function getNr(elemNr, high){
+    var Nr; // takes the input from a varable an converts it int a number
 
-    nr = parseInt(nr);
+    Nr = inputElem[elemNr];
+    Nr = Number(inputElem[elemNr].value)
+    Nr = parseInt(Nr);
+
+
     // if statment that chechs if nr is in range 1-5
-    if(nr < 1 || nr > 5){
+    if(Nr < 1 || Nr > high){
         msgElem.innerHTML = "talet är inte mellan 1-5"
-        return;
+        return null;
     }
     // if statment that chechs if nr is a number
-    if(isNaN(nr)){
+    if(isNaN(Nr)){
         msgElem.innerHTML = "inmatningen är inte ett tal"
-        return; 
+        return null; 
     }
+
+    return Nr;
+}
+
+
+// The function checks the imput of the first text feld and alters the img on the webbpage if the input was a nummber that
+function showFruit() {    
+    var fruitUrl; // stores a reffrens that is used to alter the img on the webbpage
+    var nr = getNr(1,5); // stores the return value of getNr
+
+    if(nr === null){
+        console.info("null value detected")
+        return;
+    }
+   
+    fruitUrl = document.getElementById("fruitImg");
 
     // if else if else code that checks specificly for the numbers that are used to display fruits
     if(nr == 1){
@@ -94,3 +110,16 @@ function checkName(){
 
 
 }
+
+function addFruits(){
+    var imgList;
+    var amount;
+
+    if(fruitNr == 0){
+        console.info("No fruit chosen");
+        msgElem.innerHTML = "Ingen frukt är vald" 
+        return;
+    }
+
+}
+

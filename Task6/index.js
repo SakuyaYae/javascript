69d0c9c24f0ef5093ef82f65bf8f5aabfff9fdd8
SakuyaYae <darkreimu@outlook.com>
2021-09-09 11:07:13 +0200

    alterd var names and added functionalety

--- Task6/index.js
+++ Task6/index.js
@@ -5,14 +5,15 @@ var max; // max value for sakura_sum_min_max()
 var numbers; // argument for sakura_calc_squere()
 var text; // argument for polydrom checking function sakura_check_string()
 var tracker = 0; // keeps track what backrund to chance to
-
+var sakura_elem;
 
 function init() {
-    inputElem = [];
-    inputElem[0] = document.getElementById("max")
-	inputElem[1] = document.getElementById("min")
-    inputElem[2] = document.getElementById("number")
-    inputElem[3] = document.getElementById("text")
+    input_elem = [];
+    input_elem[0] = document.getElementById("max")
+	input_elem[1] = document.getElementById("min")
+    input_elem[2] = document.getElementById("number")
+    input_elem[3] = document.getElementById("text")
+    sakura_elem = document.getElementById("sakura")
 
     document.getElementById("btn_min").onclick = sakura_min;
     document.getElementById("btn_max").onclick = sakura_max;
@@ -25,22 +26,22 @@ function init() {
 window.onload = init;
 // returns the inputed max value
 function sakura_max(){
-    max = Number(inputElem[0].value);
+    max = Number(input_elem[0].value);
     return max;
 }
 // returns the inputed min value
 function sakura_min(){
-    min = Number(inputElem[1].value);
+    min = Number(input_elem[1].value);
     return min;
 }
 // returns the inputed value for square number
 function sakura_numbers(){
-    numbers = Number(inputElem[2].value);
+    numbers = Number(input_elem[2].value);
     return numbers;
 }
 // returns the inputed text string
 function sakura_text_string(){
-  text = String(inputElem[3].value)
+  text = String(input_elem[3].value)
   return text;
 }
 
@@ -112,12 +113,12 @@ function sakura_check_string(text){
 }
 
 function sakura_alter_background(){
-
+  var changer;
 
 
   if(tracker == 0){
 
-
+    sakura_elem.style.color = "ff00ff"
     tracker = 1;
   }
 
